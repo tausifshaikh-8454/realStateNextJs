de6def11cbc4b2f99d05eba17ce5e5084fee6a5b
tausifShaikh <tausif8454@gmail.com>
2026-06-02 18:02:41 +0530

    slider change with swiper slider and update the section and marquee section added

--- src/components/Home/ClientTestimony/ClientTestimony.tsx
+++ src/components/Home/ClientTestimony/ClientTestimony.tsx
@@ -1,49 +1,103 @@
 "use client";
 
-import demo from "../../../assets/properties/prop1.webp";
-import Autoplay from "embla-carousel-autoplay";
-
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
+import testiImg from "../../../assets/testimony/div_avarta.webp";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+import "swiper/css/navigation";
+import "swiper/css";
+import "./testiStyle.css";
 
 export default function ClientTestimony() {
   return (
-    <div className="flex flex-row justify-center w-full bg-[#1A1A1A]">
-      <div className="">
-
-      </div>
-      <div className="">
-        {" "}
-        <Carousel
-          plugins={[
-            Autoplay({
-              delay: 6000,
-            }),
-          ]}
-          opts={{
-            align: "center",
-            slidesToScroll: 1,
-            loop: true,
-          }}
-          className="m-w-360 w-full overflow-hidden px-20 justify-center"
-        >
-          <CarouselContent className="m-0">
-            <CarouselItem className="basis-full lg:basis-1/3">
-              <div className="">
-                <img src={`${demo.src}`} />
-                <h5 className="">Slide 1</h5>
+    <>
+      <div className="max-w-360 w-full flex flex-row justify-center m-auto px-10 py-25 bg-[#1A1A1A] gap-30">
+        <div className="w-[50%]">
+          <h2 className="font-Roboto font-medium text-[40px]/[52px] text-white">
+            What our customers are saying us?
+          </h2>
+          <div className="grid grid-cols-2 grid-rows-1 pt-5">
+            <h5 className="font-Roboto font-medium text-[26px]/[32px] text-white">
+              10m+
+            </h5>
+            <h5 className="font-Roboto font-medium text-[26px]/[32px] text-white">
+              4.88
+            </h5>
+            <p className="font-Roboto font-regular text-[15px]/[32px] text-white">
+              Happy People
+            </p>
+            <p className="font-Roboto font-regular text-[15px]/[32px] text-white">
+              Overall rating
+            </p>
+          </div>
+        </div>
+        <div className="w-[50%]">
+          <Swiper
+            modules={[Navigation, Autoplay]}
+            slidesPerView={1}
+            navigation
+            // autoplay={{
+            //   delay: 3000,
+            //   disableOnInteraction: false,
+            // }}
+            // loop={true}
+          >
+            <SwiperSlide>
+              <div className="flex flex-col">
+                <div className="flex flex-row gap-10 items-center">
+                  <img src={testiImg.src} alt="" />
+                  <div className="">
+                    <p className="font-Roboto font-medium text-[20px] text-white">
+                      Cameron Williamson
+                    </p>
+                    <p className="font-Roboto font-regular text-[20px] text-white">
+                      Designer
+                    </p>
+                  </div>
+                </div>
+                <p className="font-Roboto font-medium text-[20px] text-white my-10">
+                  Searches for mutiplexes, property comparisons, and the loan
+                  estimator. Works great. Lorem ipsum dolor sit amet,
+                  consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                  ut labore et dores.
+                </p>
               </div>
-            </CarouselItem>
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <div className="flex flex-col">
+                <div className="flex flex-row gap-10 items-center">
+                  <img src={testiImg.src} alt="" />
+                  <div className="">
+                    <p className="font-Roboto font-medium text-[20px] text-white">
+                      Cameron Williamson
+                    </p>
+                    <p className="font-Roboto font-regular text-[20px] text-white">
+                      Designer
+                    </p>
+                  </div>
+                </div>
+                <p className="font-Roboto font-medium text-[20px] text-white my-10">
+                  Searches for mutiplexes, property comparisons, and the loan
+                  estimator. Works great. Lorem ipsum dolor sit amet,
+                  consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                  ut labore et dores.
+                </p>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </div>
-    </div>
+
+      <div className="max-w-360 w-full border-t-1 border-white bg-[#1A1A1A] px-10 py-25">
+        <div className="inline-block">
+          <img src={testiImg.src} alt="Testimonial" />
+          <img src={testiImg.src} alt="Testimonial" />
+          <img src={testiImg.src} alt="Testimonial" />
+          <img src={testiImg.src} alt="Testimonial" />
+          <img src={testiImg.src} alt="Testimonial" />
+          <img src={testiImg.src} alt="Testimonial" />
+        </div>
+      </div>
+    </>
   );
 }
